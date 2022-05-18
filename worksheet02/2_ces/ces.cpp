4efc79460fdaac212af81161b735833c5638e3e9
Dang Diep <diepda@lapnm10.in.nm.ifi.lmu.de>
2022-05-18 11:20:43 +0200

    Add the solution to worksheet 2

--- worksheet02/2_ces/ces.cpp
+++ worksheet02/2_ces/ces.cpp
@@ -3,6 +3,25 @@
 #include <atomic>	// std::atomic...
 #include <cstdint>	// uint32_t...
 
+/* compare_exchange_strong implementation */
+template<typename T>
+bool compare_exchange_strong(	std::atomic<T>*		obj,
+				T* 			expected,
+				T			val,
+				std::memory_order	success,
+				std::memory_order	failure	)
+					noexcept
+{
+	bool	res;	// contain the return value of ces
+	T	exp_prev = *expected;	// save the original value of parameter @expected
+
+	do {
+		res = atomic_compare_exchange_weak_explicit(obj, expected, val, success, failure);
+	} while (!res && exp_prev == *expected);	// retry if encountering a spurious failure
+
+	return res;
+}
+
 /* Spinlock implementation */
 class spinlock
 {
@@ -21,7 +40,9 @@ public:
                 do {
                         expected = CLEAR; 	// CAUTION!!!: 	This variable can be changed to
 						// 		SET after executing the next c_e_w
-                } while (!flag.compare_exchange_weak(expected, SET, std::memory_order_acquire));
+                } while (!compare_exchange_strong(&flag, &expected, SET,
+						std::memory_order_acquire,
+						std::memory_order_acquire));
 		// attempt to change the flag from CLEAR to SET
         }
 
@@ -48,7 +69,7 @@ void thread_entry(uint32_t tid)
 
 	std::cout << "shared_data = " << shared_data << std::endl;
 
-       	mutex.unlock(); // release the mutex lock
+       	mutex.unlock();	// release the mutex lock
 }
 
 /* Main thread's code */
